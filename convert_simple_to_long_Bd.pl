2d14555c1f02a69b7676b66fc2b023599c5f9c97
Michigan Mycology <tyjames@umich.edu>
2024-11-26 08:00:28 -0500

    Add files via upload

---
+++ convert_simple_to_long_Bd.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+# This program takes a simple file in array format and makes long list file that can be read in R
+
+# open input and output files
+open (INPUTFILE, $ARGV[0]);
+open (OUTPUTFILE, "> $ARGV[1]");
+
+if (@ARGV < 3) {
+    die "Improper number of arguments. Usage: perl convert_simple_to_long_Bd.pl infile outfile numstrains\n";
+}
+
+$numstrains=$ARGV[2] ;
+
+print OUTPUTFILE "Position\tStrain\tGenotype\n";
+$a=0;
+
+$firstline = <INPUTFILE>;
+print $firstline;
+@names = split(/\t/, $firstline);
+$names[$numstrains+1] =~ s/\s+$//;
+for ($z=1; $z<$numstrains+1; $z++)
+{
+	$strainname[$z]=$names[$z];
+	print "$strainname[$z]\t";
+} 
+
+$strainname[$numstrains] =~ s/\s+$//;
+while (<INPUTFILE>)
+{
+	@info = split(/\t/, $_);
+#	print @info;
+	$info[$numstrains] =~ s/\s+$//;
+	$data[$a][0] = $info[0];
+	for ($b=1; $b<$numstrains+1; $b++)
+	{
+#		print "info[$b+2] = $info[$b+2]\n";
+		$data[$a][$b]=$info[$b];
+#		print "data[$a][$b] = $data[$a][$b]\n";
+	}
+	$a++;
+#	print "\n";
+}
+
+$numbases=$a;
+print "numbase=$numbases";
+
+for ($c=1; $c<$numstrains+1; $c++)
+{
+	for ($d=0; $d<$numbases; $d++)
+	{
+		print OUTPUTFILE "$data[$d][0]\t$strainname[$c]\t$data[$d][$c]\n";
+#		print "$data[$d][0]\t$strainname[$c]\t$data[$d][$c]\n";
+	}
+}
